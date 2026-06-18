a7531d4c7074ad66bfa4a6adfe28981e9c19089c
ritaamaralsilva <rita.asilva93@gmail.com>
2026-06-18 23:55:39 +0100

    implement pewterCity with battle, training, gym and pokeCenter

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,13 @@
 package game;
 
 import entities.*;
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
+    private Trainer player; // atributo da classe Game
+
     public void pookemon() { // metodo do jogo, instancio aqui todos os objetos
         Scanner userInput = new Scanner(System.in);
         System.out.println("Olá! Bem vindo ao POOKÉMON!");
@@ -13,13 +17,13 @@ public class Game {
         String userGenderName = userInput.next();
 
         // pokemon starter
-        Bulbasaur bulbasaur = new Bulbasaur("Bulba", 60, 60, 40, 5);
-        Charmander charmander = new Charmander("Chaaaar", 50, 50, 55, 5);
-        Squirtle squirtle = new Squirtle("Squirrrr", 55, 55, 45, 5);
-        Pikachu pikachu = new Pikachu("Pika Pika", 65, 65, 45, 5);
+        Bulbasaur bulbasaur = new Bulbasaur("Bulba", 45, 45, 49, 5, 0);
+        Charmander charmander = new Charmander("Chaaaar", 39, 39, 52, 5, 0);
+        Squirtle squirtle = new Squirtle("Squirrrr", 44, 44, 48, 5, 0);
+        Pikachu pikachu = new Pikachu("Pika Pika", 35, 35, 55, 5, 0);
 
         // user vai ter de criar o seu Trainer aqui, usar o metodo trainer.showDetails() para mostrar os detalhes no fim
-        Trainer player = new Trainer(userPlayerName, userGenderName, 1000, null);
+        player = new Trainer(userPlayerName, userGenderName, 1000, null);
         ;
         System.out.println("... São 7:30h da manhã, o teu despertador está a tocar. O que vais fazer?");
         // criar booleano de acordar quando o despertador toca, user escolhe.
@@ -67,37 +71,103 @@ public class Game {
     public void pewterCity() {
         Scanner input = new Scanner(System.in);
 
-        // instanciar pokemon wild para os treinos
-        PokemonWild geodude = new PokemonWild("Geodude", 50, 50, 10, 6, 300);
-        PokemonWild onix = new PokemonWild("Onix", 60, 60, 25, 7, 500);
-        PokemonWild rhyhorn = new PokemonWild("Rhyhorn", 70, 70, 30, 7, 700);
-        PokemonWild pidgey = new PokemonWild("Pidgey", 30, 30, 20, 5, 250);
-        PokemonWild ratatta = new PokemonWild("Ratatta", 30, 30, 10, 5, 250);
-        PokemonWild metapod = new PokemonWild("Metapod", 60, 60, 0, 7, 350);
-        PokemonWild caterpie = new PokemonWild("Caterpie", 40, 40, 15, 5, 250);
-        PokemonWild spearrow = new PokemonWild("Spearrow", 40, 40, 15, 6, 300);
+        boolean inCity = true;
+        while (inCity) {
+            System.out.println("\n Chegaste a Pewter City, o que vais fazer a seguir? ");
+            System.out.println("1. Enfrentar o Brock, o Gym Leader");
+            System.out.println("2. Comprar itens na Pooke Shop");
+            System.out.println("3. Treinar o teu Pookémon");
+            System.out.println("4. Ir ao PookéCenter");
+            int cityChoice = input.nextInt();
+            switch (cityChoice) {
+                case 1:
+                    Gym pewterCityGym = new Gym("Pewter City Gym", 12, 1386);
+                    pewterCityGym.addPokemon(new PokemonWild("Geodude", 50, 50, 80, 12, 300, 500));
+                    pewterCityGym.addPokemon(new PokemonWild("Onix", 35, 35, 45, 14, 500, 1000));
 
-        System.out.println("\nBem-vindo a Pewter City!");
-        System.out.println("Chegaste a Pewter City, o que vais fazer a seguir? ");
-        System.out.println("1. Enfrentar o Brock, o Gym Leader");
-        System.out.println("2. Comprar itens na Pooke Shop");
-        System.out.println("3. Treinar o teu Pookémon");
-        int cityChoice = input.nextInt();
-        switch (cityChoice) {
-            case 1:
-                System.out.println("Escolheste enfrentar o Gym Leader Brock! Boa sorte!");
-                break;
-            case 2:
-                System.out.println("Escolheste ir à loja abastecer!");
-                break;
-            case 3:
-                System.out.println("Escolheste ir treinar o teu Pookémon!");
-                break;
-            default:
-                System.out.println("Escolheste uma opção inválida!");
-                return;
+                    if (player.getPokemonInUse().getLevel() < pewterCityGym.getMinLevelToBattle()) {
+                        System.out.println("O teu Pookémon é fraco demais! Precisas de estar pelo menos no nível "
+                                + pewterCityGym.getMinLevelToBattle() + " para entrar neste gym.");
+                        break;
+                    }
+
+                    System.out.println("Escolheste enfrentar o Gym Leader Brock!");
+                    boolean gymWon = true;
+
+                    for (Pokemon gymPokemon : pewterCityGym.getPokemonGym()) {
+                        System.out.println("Brock enviou o " + gymPokemon.getName() + "!");
+                        boolean won = player.getPokemonInUse().pokemonBattle(gymPokemon);
+
+                        if (!won) {
+                            gymWon = false;
+                            inCity = false;
+                            break;
+                        }
+                        player.showDetails();
+                    }
+
+                    if (gymWon) {
+                        System.out.println("Parabéns! Derrotaste o Brock e ganhaste o Boulder Badge!");
+                        player.addCoins(pewterCityGym.getReward());
+                        player.addGymBadge();
+                        inCity = false; // avança para a próxima cidade
+                    }
+                    break;
+                case 2:
+                    System.out.println("Escolheste ir à loja abastecer!");
+                    break;
+                case 3:
+                    System.out.println("Escolheste ir treinar o teu Pookémon!");
+                    boolean training = true;
+
+                    while (training) {
+                        // instanciar pokemon wild para os treinos
+                        PokemonWild geodude = new PokemonWild("Geodude", 50, 50, 10, 6, 300, 200);
+                        PokemonWild onix = new PokemonWild("Onix", 60, 60, 25, 7, 500, 300);
+                        PokemonWild rhyhorn = new PokemonWild("Rhyhorn", 70, 70, 30, 7, 700, 350);
+                        PokemonWild pidgey = new PokemonWild("Pidgey", 30, 30, 20, 5, 250, 200);
+                        PokemonWild ratatta = new PokemonWild("Ratatta", 30, 30, 10, 5, 250, 200);
+                        PokemonWild metapod = new PokemonWild("Metapod", 60, 60, 0, 7, 350, 150);
+                        PokemonWild caterpie = new PokemonWild("Caterpie", 40, 40, 15, 5, 250, 200);
+                        PokemonWild spearow = new PokemonWild("Spearow", 40, 40, 15, 6, 300, 250);
+
+                        PokemonWild[] wildPokemonPewterCity = {geodude, onix, rhyhorn, pidgey, ratatta, metapod, caterpie, spearow};
+
+                        Random random = new Random();
+                        PokemonWild enemy = wildPokemonPewterCity[random.nextInt(wildPokemonPewterCity.length)];
+
+                        System.out.println("Um " + enemy.getName() + " selvagem apareceu!");
+                        boolean winBattle = player.getPokemonInUse().pokemonBattle(enemy);
+
+                        if (!winBattle) { // se perde pokemonBattle, acaba os ciclos porque game over
+                            training = false;
+                            inCity = false;
+                            System.out.println("Ohhh, o teu " + player.getPokemonInUse().getName() + " era tão fraquinho que morreu... Game Over, melhor sorte da próxima!");
+                        } else if (winBattle) {
+                            player.addCoins(enemy.getCoins()); // vai buscar os coins do pokemon inimigo para atribuir ao player
+                            player.showDetails(); // mostra o estado atual do jogador
+
+                            System.out.println("\nO que queres fazer agora?");
+                            System.out.println("1. Continuar a treinar");
+                            System.out.println("2. Voltar ao menu da cidade");
+                            int trainingChoice = input.nextInt();
+                            if (trainingChoice == 2) {
+                                training = false; // volta ao menu da cidade
+                            }
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Bem-vindo ao PookéCenter!");
+                    System.out.println("A Nurse Joy trata do teu Pookémon...");
+                    player.getPokemonInUse().healPokemon();
+                    player.showDetails();
+                    break;
+                default:
+                    System.out.println("Escolheste uma opção inválida!");
+                    break;
+            }
         }
     }
 }
-
 
