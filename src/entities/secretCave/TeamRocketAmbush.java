3a40a53b33ceae5a6586d50f262f39a2e59e941f
ritaamaralsilva <rita.asilva93@gmail.com>
2026-06-20 16:18:14 +0100

    criacao de sub classe Team Rocket Ambush para ser um evento aleatorio que pode acontecer no modo alternativo do jogo, secret cave

--- src/entities/secretCave/TeamRocketAmbush.java
+++ src/entities/secretCave/TeamRocketAmbush.java
@@ -1,5 +1,28 @@
-package game.events;
+package entities.secretCave;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class TeamRocketAmbush {
-    
+    // Declaração da ArrayList
+    private ArrayList<String> members;
+    private Random random;
+
+    public TeamRocketAmbush() {
+        this.random = new Random();
+        this.members = new ArrayList<>();
+
+        this.members.add("Jessie");
+        this.members.add("James");
+        this.members.add("um Grunt da Team Rocket");
+        this.members.add("Butch");
+        this.members.add("Cassidy");
+        this.members.add("Meowth ladrão");
+    }
+
+    public String getMember() {
+        // o random aqui vai escolher uma posição do array list que tem os membros que adicionei
+        int chosenTeamRocketMember = random.nextInt(this.members.size());
+        return this.members.get(chosenTeamRocketMember);
+    }
 }
