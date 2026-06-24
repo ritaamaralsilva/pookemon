95e80aa02c718a6f85dd133f60027f09edd8e982
ritaamaralsilva <rita.asilva93@gmail.com>
2026-06-24 17:02:26 +0100

    criacao de ficheiro UserInput e metodo checkForUserInputError para optimizar o catch and try (procura de mismatch exception) que aplico a TODOS os menus do game.java onde há user input através do scanner, de forma a que o programa não bloqueie caso o user escreva uma letra ou símbolo quando é esperado um nr inteiro. isto pretende optimizar o meu código e poupar muitas linhas de código que pode e deve ser reutilizado do

---
+++ src/game/UserInput.java
@@ -0,0 +1,27 @@
+package game;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInput {
+    /**
+     * Este método serve para centralizar o catch e try (deteção de erro) do user input com o Scanner
+     * Ou seja, em TODOS os menus do jogo, caso o user reponda na consola com string ou um símbolo em vez de numero inteiro como é pedido
+     * O programa não vai bloquear e assim o jogo continua
+     * Fiz desta forma para evitar o uso repetido de código no jogo todo, e assim só chamo a função sempre que há menus com user input
+     * @param input
+     * @return
+     */
+    public static int checkForUserInputError(Scanner input) {
+        while (true) {
+            try {
+                int value = input.nextInt(); // verifica se o input é inteiro
+                input.nextLine(); // se for int, avança
+                return value; // retorna o valor do input para avançar nos menus
+            } catch (InputMismatchException e) {
+                input.nextLine(); // caso user tenha introduzido string em vez de inteiro
+                ConsoleColors.error("Opção inválida! Introduz um número."); // erro default para não quebrar o programa
+            }
+        }
+    }
+}
